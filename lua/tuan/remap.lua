f9496ada91438ed1b26b7844b824a625867b5f78
Cao Anh Tuan <caotuanyism@gmail.com>
2025-05-06 23:02:21 +0200

    Remove an useless remap

--- lua/tuan/remap.lua
+++ lua/tuan/remap.lua
@@ -14,7 +14,6 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- greatest remap ever
 vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<leader>P", [["+p]])
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 vim.keymap.set("i", "<C-c>", "<Esc>")
