9f70ef476f1add14f581b4831cd890d0b2d55818
Cao Anh Tuan <caotuanyism@gmail.com>
2025-05-06 10:28:56 +0200

    added lualine plugin

--- lua/tuan/default_config.lua
+++ lua/tuan/default_config.lua
@@ -1,3 +1,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- Highlight current line number
+vim.api.nvim_set_hl(0, 'LineNr', { fg = '#ffaa00', bold = true })
+
+-- Highlight relative numbers above and below
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#888888' })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#888888' })
